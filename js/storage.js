ceb0a36b73911b147013105caf579ce91ecd3c77
ChavaGG <A01639338@tec.mx>
2025-04-07 12:08:50 -0600

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,8 +1,20 @@
-// Funciones para manejar el almacenamiento
-function saveDailyObjectives() {
-  localStorage.setItem('dailyObjectives', JSON.stringify(dailyObjectives));
+// Función para obtener los objetivos desde el localStorage o inicializarlos
+function getObjectives(type) {
+  const objectives = localStorage.getItem(type);
+  return objectives ? JSON.parse(objectives) : [];
 }
 
+// Función para guardar los objetivos en el localStorage
+function saveObjectives(type, objectives) {
+  localStorage.setItem(type, JSON.stringify(objectives));
+}
+
+// Guardar objetivos diarios
+function saveDailyObjectives() {
+  saveObjectives('dailyObjectives', dailyObjectives);
+}
+
+// Guardar objetivos semanales
 function saveWeeklyObjectives() {
-  localStorage.setItem('weeklyObjectives', JSON.stringify(weeklyObjectives));
+  saveObjectives('weeklyObjectives', weeklyObjectives);
 }
